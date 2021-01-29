6b728776b6d4f3ba468158166aca7f4c37130f74
Kirsh Rustagi <54409969+krishrustagi@users.noreply.github.com>
2021-01-29 23:46:39 +0530

    Prediction of Salaries

--- linear_regression.py
+++ linear_regression.py
@@ -4,6 +4,7 @@
 from sklearn.linear_model import LinearRegression
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
+import numpy as np
 import pandas as pd
 
 
@@ -15,8 +16,12 @@ X = pricing[["YearsExperience"]]
 Y = pricing[["Salary"]]
 
 
+X = np.reshape(np.array(X), (len(X), 1))
+Y = np.reshape(np.array(Y), (len(Y), 1))
+
+
 X_train, X_test, Y_train, Y_test = train_test_split(
-    X, Y, test_size=0.2, random_state=20)  # spliting test and train data
+    X, Y, test_size=0.2)  # spliting test and train data
 
 
 plt.scatter(X, Y)
@@ -39,4 +44,17 @@ plt.plot(X_train, reg.predict(X_train), color='black')
 plt.plot(X_test, reg.predict(X_test), color='green')
 plt.xlabel("Years of Experience (in Years)")
 plt.ylabel("Salary (in $)")
-plt.show()  # final plot with linear regression
+plt.show()  # final plot with
+
+
+# predicting few salaries based on years of experience
+
+
+# predicition no. 1
+pred1 = reg.predict(np.array([[13]]))  # 13 years of experience
+print(pred1)
+
+
+# prediction no. 2
+pred2 = reg.predict(np.array([[7.3]]))
+print(pred2)
